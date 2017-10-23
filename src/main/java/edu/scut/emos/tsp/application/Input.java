5cc24637ed4631fb1074cba0036cd7b50b81834e
psy-mas <psy-mas@Master>
2017-10-23 19:58:21 +0800

    项目提交

---
+++ src/main/java/edu/scut/emos/tsp/application/Input.java
@@ -0,0 +1,10 @@
+package edu.scut.emos.tsp.application;
+class Input {
+	private final String key;
+	private final String meg;
+   Input(String message){
+	   key=message.getKey();
+	   msg=message.getMessage();
+   }
+   
+}
---
+++ src/main/java/edu/scut/emos/tsp/application/TestApp.java
@@ -0,0 +1,17 @@
+package edu.scut.emos.tsp.application;
+import org.apache.samza.application.StreamApplication;
+import org.apache.samza.config.Config;
+import org.apache.samza.operators.MessageStream;
+import org.apache.samza.operators.OutputStream;
+import org.apache.samza.operators.StreamGraph;
+import org.apache.samza.operators.functions.JoinFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+
+public class TestApp implements StreamApplication {
+	
+}
